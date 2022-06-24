4ea1c28c17562d2acdc22c38b8ca7329ba0547b2
diego vila <ruahman@gmail.com>
2022-06-24 12:45:16 -0400

    made some changes to init.el

--- init.el
+++ init.el
@@ -1,5 +1,9 @@
 (setq inhibit-startup-message t)
 
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
 (setq-default truncate-lines 1)
 
 (set-fringe-mode 10)        ; Give some breathing room
@@ -32,6 +36,7 @@
                          ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
+
 (unless package-archive-contents
  (package-refresh-contents))
 
@@ -42,38 +47,45 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+(use-package general :ensure t)
+
 (use-package swiper :ensure t)
+
+(use-package counsel :ensure t
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+	 ("C-x f" . counsel-find-file))
+  )
+
+(general-define-key
+  "C-s" 'swiper
+  )
+
+;(use-package hydra)
+
+;(defhydra hydra-text-scale (:timeout 4)
+;  "scale text"
+;  )
+
 (use-package ivy
   :diminish
-  :bind (("C-s" . swiper)
-         :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)	
-         ("C-l" . ivy-alt-done)
-         ("C-j" . ivy-next-line)
-         ("C-k" . ivy-previous-line)
-         :map ivy-switch-buffer-map
-         ("C-k" . ivy-previous-line)
-         ("C-l" . ivy-done)
-         ("C-d" . ivy-switch-buffer-kill)
-         :map ivy-reverse-i-search-map
-         ("C-k" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill))
-  :config
-  (ivy-mode 1))
+  :config (ivy-mode 1))
 
-;(use-package ivy-rich
-;  :init
-;  (ivy-rich-mode 1))
+(use-package ivy-rich
+  :ensure t
+  :init
+  (ivy-rich-mode 1))
 
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
 
 (use-package which-key
-  :defer 0
+  :init (which-key-mode)
   :diminish which-key-mode
-  :config
-  (which-key-mode)
-  (setq which-key-idle-delay 0.3))
+  :config (which-key-mode)
+  )
+
+(use-package magit)
 
 (use-package doom-modeline
   :ensure t
@@ -92,32 +104,22 @@
   :config
   (dashboard-setup-startup-hook))
 
-;(use-package general
-;  :ensure t)
-
-;(general-auto-unbind-keys)
-
-;(general-define-key
-; :prefix "SPC" 
-; "SPC" 'execute-extended-command 
-; )
 
 
 
 
-;;------------ this was automatically added -----------------------------
+
+;;------------ this was automatically added from any custome changes made in emacs  -----------------------------
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- (menu-bar-mode -1)          ; Disable the menu bar
- (tool-bar-mode -1)          ; Disable the toolbar
- (scroll-bar-mode -1)        ; Disable visible scrollbar
- (tooltip-mode -1)           ; Disable tooltips
- '(package-selected-packages '(swiper use-package ivy))
- )
+ '(custom-safe-themes
+   '("5f19cb23200e0ac301d42b880641128833067d341d22344806cdad48e6ec62f6" "353ffc8e6b53a91ac87b7e86bebc6796877a0b76ddfc15793e4d7880976132ae" "47db50ff66e35d3a440485357fb6acb767c100e135ccdf459060407f8baea7b2" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "745d03d647c4b118f671c49214420639cb3af7152e81f132478ed1c649d4597d" "c4063322b5011829f7fdd7509979b5823e8eea2abf1fe5572ec4b7af1dd78519" "3d47380bf5aa650e7b8e049e7ae54cdada54d0637e7bac39e4cc6afb44e8463b" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" default))
+ '(nil nil t)
+ '(package-selected-packages '(swiper use-package ivy)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
