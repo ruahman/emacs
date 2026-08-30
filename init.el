56f79bdcc951ceb08157da19b40b8739023f5d2a
dego_ <dego_vila@yahoo.com>
2026-08-29 22:06:07 -0400

    use-package

--- init.el
+++ init.el
@@ -1,5 +1,47 @@
+;; setup username
+(setq user-full-name "Diego Vila")
 
-;; setup package
+;; set font
+(set-face-attribute 'default nil
+                    :font "Hack Nerd Font"
+                    :height 140)
+
+;; stop making backup files
+(setq make-backup-files nil)
+
+;; take out startup screen
+(setq inhibit-startup-message t)
+
+;; cause scroll bar, tool bar, and menu to disapear
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+;; get ride of word wrapping
+(setq-default truncate-lines 1)
+
+;; get ride of the fringes from side of window
+(set-fringe-mode 10)      
+
+;; Set up the visible bell, flashes
+(setq visible-bell t)
+
+;; show line numbers
+(global-display-line-numbers-mode t)
+
+;; show column number in mode line
+(column-number-mode)
+
+;; enable fido
+(fido-vertical-mode 1)
+
+;; Enable which-key (Built-in for Emacs 30+)
+(which-key-mode 1)
+
+;; dired
+(setq dired-listing-switches "-alh --group-directories-first")
+
+;; Setup package
 (require 'package)
 
 (setq package-archives
@@ -22,6 +64,7 @@
 
 ;; load use-package
 (require 'use-package)
+
 ;; install package if not found
 (setq use-package-always-ensure t) 
 
@@ -89,149 +132,71 @@
   (org-mode . org-superstar-mode))
 
 
-;;;; Install org-roam
-(unless (package-installed-p 'org-roam)
-  (package-install 'org-roam))
-
-;; where roam looks for notes (file-truename handles symlinks)
-(setq org-roam-directory (file-truename "~/org/4_areas/kingdom/personal-study"))
-
-;; keep the SQLite database in sync automatically
-(org-roam-db-autosync-mode)
-
-;; keybindings (C-c r prefix, which-key will show these)
-(global-set-key (kbd "C-c r f") #'org-roam-node-find)
-;; (global-set-key (kbd "C-c r i") #'org-roam-node-insert)
-;; (global-set-key (kbd "C-c r t") #'org-roam-buffer-toggle) ; show backlinks
-;; (global-set-key (kbd "C-c r c") #'org-roam-capture) ; use caputere to take a note
-
-;; how to show search results for org-roam-node-find
-(setq org-roam-node-display-template
-      (concat "${title} " (propertize "${tags}" 'face 'org-tag)))
-
-;;;; Install denote
-(unless (package-installed-p 'denote)
-  (package-install 'denote))
-
-;; where notes live (matches your existing org-directory)
-(setq denote-directory (expand-file-name "~/org/5_resources"))
-
-;; save denote automatically
-(setq denote-save-buffers 1)
-
-;; (setq denote-file-type 'markdown-yaml)'
-(setq denote-prompts '(title keywords file-type))
-
-(setq denote-known-keywords '("meta" "tmp" "draft"))
-
-;; (global-set-key (kbd "C-c d n") #'denote)                 ; new note
-;; (global-set-key (kbd "C-c d r") #'denote-rename-file)     ; rename to denote scheme
-;; (global-set-key (kbd "C-c d l") #'denote-link)            ; insert link to a note
-;; (global-set-key (kbd "C-c d b") #'denote-backlinks)       ; show backlinks
-;; (global-set-key (kbd "C-c d o") #'denote-open-or-create)  ; jump to / create note
-;; (global-set-key (kbd "C-c d k") #'denote-rename-file-keywords) ; edit keywords
-
-;; fontify the filename fields (date / title / keywords) in dired
-(add-hook 'dired-mode-hook #'denote-dired-mode)
+;; Setup org-roam
+(use-package org-roam
+  :init
+  ;; where roam looks for notes
+  (setq org-roam-directory (file-truename "~/org/4_areas/kingdom/personal-study"))
+  :config
+  ;; keep the SQLite database in sync automatically
+  (org-roam-db-autosync-mode)
+  ;; how to show search results for org-roam-node-find
+  (setq org-roam-node-display-template
+        (concat "${title} " (propertize "${tags}" 'face 'org-tag)))
+  :bind
+  ("C-c n f" . org-roam-node-find)
+  ("C-c n c" . org-roam-capture)
+  ("C-c n t" . org-roam-buffer-toggle))
 
 
-;;;; Install doom-themes
-(unless (package-installed-p 'doom-themes)
-  (package-install 'doom-themes))
+;; Set denote
+(use-package denote
+  :init
+  ;; where notes live (matches your existing org-directory)
+  (setq denote-directory (expand-file-name "~/org/5_resources"))
+  :config
+  ;; save denote automatically
+  (setq denote-save-buffers 1)
+  ;; (setq denote-file-type 'markdown-yaml)'
+  (setq denote-prompts '(title keywords file-type))
+  (setq denote-known-keywords '("meta" "tmp" "draft"))
+  :bind
+  ("C-c d" . denote)
+  :hook
+  (dired-mode . denote-dired-mode))
 
-;; load doom-one theme
-(load-theme 'doom-one t)
 
-;;;; Install emacs dashboard
-(unless (package-installed-p 'dashboard)
-  (package-install 'dashboard))
+;; Setup doom-themes
+(use-package doom-themes
+  :config
+  (load-theme 'doom-one t))
 
-;;;; Install nerd-icons
-(unless (package-installed-p 'nerd-icons)
-  (package-install 'nerd-icons))
+;; Setup nerd-icons
+(use-package nerd-icons)
 
-;; run M-x nerd-icons-install-fonts
+;; Setup nerd-icons-dired
+(use-package nerd-icons-dired)
 
-;;;; Install nerd-icons-dired
-(unless (package-installed-p 'nerd-icons-dired)
-  (package-install 'nerd-icons-dired))
-
-;; load dashboard
-(require 'dashboard)
-(dashboard-setup-startup-hook)
-
-;; set heading icons
-(setq dashboard-set-heading-icons t)
-
-;; set file icons
-(setq dashboard-set-file-icons t)
-
-;; Install all-the-iconsw
-(unless (package-installed-p 'all-the-icons)
-  (package-install 'all-the-icons))
-
+(use-package all-the-icons)
 ;; Run once:
 ;; M-x all-the-icons-install-fonts
 
-;;;; Install evil
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
+;; Setup dashboard
+(use-package dashboard
+  :config
+  (dashboard-setup-startup-hook)  
+  ;; set heading icons
+  (setq dashboard-set-heading-icons t)
+  ;; set file icons
+  (setq dashboard-set-file-icons t))
 
-;;;; Install magit
-(unless (package-installed-p 'magit)
-  (package-install 'magit))
+;; Setup evil
+(use-package evil)
 
-;; Install swiper
-;;(unless (package-installed-p 'swiper)
-;;  (package-install 'swiper))
-
-;;(global-set-key (kbd "C-s") 'swiper)
-;;(define-key minibuffer-local-map (kbd "C-n") 'next-line)
-;;(define-key minibuffer-local-map (kbd "C-p") 'previous-line)
+;; Setup magit
+(use-package magit)
 
 
-;; setup username
-(setq user-full-name "Diego Vila")
-
-;; set font
-(set-face-attribute 'default nil
-                    :font "Hack Nerd Font"
-                    :height 140)
-
-;; stop making backup files
-(setq make-backup-files nil)
-
-;; take out startup screen
-(setq inhibit-startup-message t)
-
-;; cause scroll bar, tool bar, and menu to disapear
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-
-;; get ride of word wrapping
-(setq-default truncate-lines 1)
-
-;; get ride of the fringes from side of window
-(set-fringe-mode 10)      
-
-;; Set up the visible bell, flashes
-(setq visible-bell t)
-
-;; show line numbers
-(global-display-line-numbers-mode t)
-
-;; show column number in mode line
-(column-number-mode)
-
-;; enable fido
-(fido-vertical-mode 1)
-
-;; Enable which-key (Built-in for Emacs 30+)
-(which-key-mode 1)
-
-;; dired
-(setq dired-listing-switches "-alh --group-directories-first")
 
 ;; Disable line numbers for some modes
 (dolist (mode '(org-mode-hook
@@ -250,14 +215,12 @@
 
 
 
-;; add icons to dired
-(require 'nerd-icons-dired)
-
 (add-hook 'dired-mode-hook #'nerd-icons-dired-mode)
 
 ;; Enable line highlighting only in programming and text modes
 ;; Highlight the current line only when editing Emacs Lisp files
-(add-hook 'emacs-lisp-mode-hook #'hl-line-mode)(add-hook 'elisp-mode-hook #'hl-line-mode)
+(add-hook 'emacs-lisp-mode-hook #'hl-line-mode)
+(add-hook 'elisp-mode-hook #'hl-line-mode)
 (add-hook 'markdown-mode-hook #'hl-line-mode)
 (add-hook 'org-mode-hook #'flyspell-mode)
 
